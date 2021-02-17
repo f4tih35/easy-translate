b3637d39668eae0ce0255be24e0e7ff70ca1f995
Fatih Melih Ersoy <fatihmelih9@hotmail.com>
2021-02-17 09:19:05 +0300

    api_key, readme
    add installation and seperate api_key from code

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # easy-translate
+
+### Installation
+
+1. Get a free API Key at [https://translate.yandex.com/developers/keys](https://translate.yandex.com/developers/keys)
+2. Clone the repo
+   ```sh
+   git clone https://github.com/f4tih35/easy-translate.git
+   ```
+3. Change API Key with yours from 2nd line of popup.js
+   ```sh
+   https://github.com/f4tih35/easy-translate/blob/master/src/popup/popup.js
+   API_KEY = ...
+   ```
+4. Open Chrome and go to [chrome://extensions/](chrome://extensions/)
+
+5. Click "Load unpacked" and select easy-translate folder which you downloaded
+
+#### Screenshots
+
+![1](Screenshots/1.png?raw=true "1")
+
+![2](Screenshots/2.png?raw=true "2")
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,12 +1,12 @@
 let langs = {"de-en":"(German-English)","de-ru":"(немецкий-русский)","de-tr":"(Almanca-Türkçe)","en-de":"(Englisch-Deutsch)","en-ru":"(английский-русский)","en-tr":"(İngilizce-Türkçe)","ru-de":"(Russisch-Deutsch)","ru-en":"(Russian-English)","ru-tr":"(Rusça-Türkçe)","tr-de":"(Türkisch-Deutsch)","tr-en":"(Turkish-English)","tr-ru":"(Турецкий-русский)"};
-
+const API_KEY = "trnsl.1.1.20200427T141024Z.76527e5ee1a0c664.75f4ea4cac82a37949b4da59c61115fd7888651c";
 
 
 
 let myFunc = function(wordx,langsx){
     let xhr = new XMLHttpRequest(),
     method = "GET",
-    url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200427T141024Z.76527e5ee1a0c664.75f4ea4cac82a37949b4da59c61115fd7888651c&text=${wordx}&lang=${langsx}`;
+    url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${API_KEY}&text=${wordx}&lang=${langsx}`;
     xhr.open(method, url, true);
     xhr.onreadystatechange = function () {
     if(xhr.readyState === XMLHttpRequest.DONE) {
